ac7f4b07b73cecb20a71198c998faaa9718b6e1b
Yaroslav Opanasenko <super_opanas@ukr.net>
2018-05-03 15:44:15 +0300

    lessons

--- Tests/test.py
+++ Tests/test.py
@@ -1,2 +1,14 @@
-A = (1, 2, 3)
-print(A)
+def print_matrix(pisun):
+    for i in range(len(pisun)):
+        print(pisun[i])
+
+A = [[1, 2, 3], [3, 4, 5]]
+B = [[1, 5, 8], [4, 8, 2]]
+
+C = [[0]*6 in range(2)]
+
+print_matrix(C)
+
+'''def union_matrix(A, B):
+    for i in range(len(A)):
+        for j in range(2 * len(A)):'''
