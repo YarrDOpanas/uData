8d3d587f29174da2241307d3f7d1a8cfaef0960e
Yaroslav Opanasenko <super_opanas@ukr.net>
2018-06-21 14:09:56 +0300

    laba4

--- Numerical methods/Laba4/main.py
+++ Numerical methods/Laba4/main.py
@@ -25,10 +25,17 @@ print("z:", z)
 if r == min(len(A), len(A[0])):
     y = z / E
     x = V.T @ y
-    print("Our system is compatible. X = ", x)
+    print("Our system is definite. X = ", x)
 else:
     y = np.zeros(len(A[0]))
     y[:r] = z[:r] / E[E > np.finfo(np.float32).eps]
-    x = V.T @ y
-    print("Our system is not compatible. it's pseudo-resolves = ", x)
-print("r = A*x - b=\n", A @ x - b)
+    x = x = V.T @ y
+    if abs(z[r:-1]) < np.finfo(np.float32).eps:
+        print("Our system is compatible.\n X = ",end = "")
+        for i in range(r):
+            print(round(y[i], 3), " * ",V.T[i],end = " + ")
+        for i in range(r, len(A[0])):
+            print('c'+str(i), " * ", V.T[i],end = " + ")
+    else:
+        print("Our system is not compatible. it's pseudo-resolves = ", x)
+print("\nr = A*x - b=\n", A @ x - b)
